a90998135dc67c203ad339a10c38e3c18621f766
Phat Panhareach <panhareach.phat@student.cadt.edu.kh>
2023-03-04 15:01:29 +0700

    add: app logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/x-icon" href="/images/logo/codeada.png">
     <title>{{ config('app.name', 'CodeAda') }}</title>
 
     <!-- Fonts -->
